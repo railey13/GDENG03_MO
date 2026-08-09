bc448c8d7cd3adc184e278f76650402f9e11d2da
TheEnabled <sol_tabboga@dlsu.edu.ph>
2026-08-09 14:56:20 +0800

    fix: add DirectXTex prebuilt libs and fix vcxproj linker settings
    - Add DirectXTex.lib for x64/x86 Debug+Release (downloaded from official NuGet)
    - Fix .gitignore to not exclude Libs/**/*.lib so libs survive git clone
    - Remove non-existent DirectXTexD.lib from vcxproj AdditionalDependencies
    - Fix AdditionalLibraryDirectories to point to correct x64/Debug and x64/Release subdirs
    - Fix DirectXTex.h WIC function signatures: DWORD flags -> WIC_FLAGS to match compiled lib

--- Libs/DirectXTex/include/DirectXTex.h
+++ Libs/DirectXTex/include/DirectXTex.h
@@ -388,31 +388,31 @@ namespace DirectX
     // WIC operations
     HRESULT __cdecl LoadFromWICMemory(
         _In_reads_bytes_(size) const void* pSource, _In_ size_t size,
-        _In_ DWORD flags,
+        _In_ WIC_FLAGS flags,
         _Out_opt_ TexMetadata* metadata, _Out_ ScratchImage& image,
         _In_opt_ std::function<void __cdecl(IWICMetadataQueryReader*)> getMQR = nullptr);
     HRESULT __cdecl LoadFromWICFile(
-        _In_z_ const wchar_t* szFile, _In_ DWORD flags,
+        _In_z_ const wchar_t* szFile, _In_ WIC_FLAGS flags,
         _Out_opt_ TexMetadata* metadata, _Out_ ScratchImage& image,
         _In_opt_ std::function<void __cdecl(IWICMetadataQueryReader*)> getMQR = nullptr);
 
     HRESULT __cdecl SaveToWICMemory(
-        _In_ const Image& image, _In_ DWORD flags, _In_ REFGUID guidContainerFormat,
+        _In_ const Image& image, _In_ WIC_FLAGS flags, _In_ REFGUID guidContainerFormat,
         _Out_ Blob& blob, _In_opt_ const GUID* targetFormat = nullptr,
         _In_opt_ std::function<void __cdecl(IPropertyBag2*)> setCustomProps = nullptr);
     HRESULT __cdecl SaveToWICMemory(
         _In_count_(nimages) const Image* images, _In_ size_t nimages,
-        _In_ DWORD flags, _In_ REFGUID guidContainerFormat,
+        _In_ WIC_FLAGS flags, _In_ REFGUID guidContainerFormat,
         _Out_ Blob& blob, _In_opt_ const GUID* targetFormat = nullptr,
         _In_opt_ std::function<void __cdecl(IPropertyBag2*)> setCustomProps = nullptr);
 
     HRESULT __cdecl SaveToWICFile(
-        _In_ const Image& image, _In_ DWORD flags, _In_ REFGUID guidContainerFormat,
+        _In_ const Image& image, _In_ WIC_FLAGS flags, _In_ REFGUID guidContainerFormat,
         _In_z_ const wchar_t* szFile, _In_opt_ const GUID* targetFormat = nullptr,
         _In_opt_ std::function<void __cdecl(IPropertyBag2*)> setCustomProps = nullptr);
     HRESULT __cdecl SaveToWICFile(
         _In_count_(nimages) const Image* images, _In_ size_t nimages,
-        _In_ DWORD flags, _In_ REFGUID guidContainerFormat,
+        _In_ WIC_FLAGS flags, _In_ REFGUID guidContainerFormat,
         _In_z_ const wchar_t* szFile, _In_opt_ const GUID* targetFormat = nullptr,
         _In_opt_ std::function<void __cdecl(IPropertyBag2*)> setCustomProps = nullptr);
 
